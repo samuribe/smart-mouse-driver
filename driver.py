4d4c5a0b219ac27ad65daf63fcacd16955e3067d
Property404 <martinez.dagan@gmail.com>
2017-03-25 21:31:19 -0400

    FUCK PASTING

--- driver.py
+++ driver.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python3
 import serial
-import pyperclip
+import xerox
+import subprocess
+import os
+from sys import platform
 from pymouse import PyMouse
+if platform == "darwin":
+	from AppKit import*
 port = "/dev/cu.usbmodemFA141"
 def readline(a_serial, eol=b'\n\n'):
     leneol = len(eol)
@@ -14,9 +20,30 @@ def readline(a_serial, eol=b'\n\n'):
         else:
             break
     return bytes(line)
-def replyPaste():
-	clipboard = pyperclip.paste()
-	
+def replyCopy(ser):
+	clipboard = xerox.paste()
+	ser.write("P")
+	for b in bytearray(clipboard, "UTF-8"):
+		ser.write(b)
+	ser.write(chr(0))
+def clipboard_paste():
+	if(platform == "darwin"):
+		stdoutdata = subprocess.getoutput("pbpaste")
+		return stdoutdata
+	else:
+		return xerox.paste()
+def clipboard_copy(x):
+	if(platform == "drwin"):
+		pb = NSPasteboard.generalPasteboard()
+		pb.clearContents()
+		a = NSArray.arrayWithObject_("hello world")
+		pb.writeObjects_(a)
+	else:
+		return xerox.copy()
+print(xerox.paste())
+clipboard_copy("weak")
+print(clipboard_paste())
+exit()
 def main():
 	ser = serial.Serial(
 	port = port,
@@ -41,9 +68,16 @@ def main():
 		if(len(ray)>2 and ray[0]==109):
 			mouse.move(ray[1]*screen_width, ray[2]*screen_height)
 		elif(len(ray)>1 and ray[0]==ord('C')):
-			print("CLICK")
 			pos = mouse.position()
 			mouse.click(pos[0], pos[1]);
+		elif(len(ray)>2 and ray[0]==ord('p')):
+			print("PASTE MOTHERFUCKER")
+			#clipboard.copy(ray.decode("UTF-8"));
+			copy_to_clipboard(ray.decode("UTF-8"));
+			input()
+		elif(len(ray)>0 and ray[0]==ord('c')):
+			print("COPY MOTHERFUCKER")
+			replyCopy(ser)
 		print(ray);
 	
 if __name__ == "__main__":
