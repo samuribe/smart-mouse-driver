ca6da77e0ac411a028a4a283aa7583a40fe60646
kingban1 <sam.urigav@gmail.com>
2017-03-25 19:16:27 -0500

    mouseListener prints USB info

---
+++ mouseListener.py
@@ -0,0 +1,14 @@
+import pyUSB
+
+def main:
+    busses = usb.busses()
+    for bus in busses:
+        devices = bus.devices
+    for dev in devices:
+        print repr(dev)
+        print "Device:", dev.filename
+        print "  idVendor: %d (0x%04x)" % (dev.idVendor, dev.idVendor)
+        print "  idProduct: %d (0x%04x)" % (dev.idProduct, dev.idProduct)
+        print "Manufacturer:", dev.iManufacturer
+        print "Serial:", dev.iSerialNumber
+        print "Product:", dev.iProduct
